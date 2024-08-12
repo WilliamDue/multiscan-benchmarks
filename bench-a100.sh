5a0b3785b133a08105b4b26c9236d09bd27545e5
mcj284 <mcj284@hendrixfut01fl.unicph.domain>
2024-08-12 14:09:08 +0200

    These were not needed.

--- bench-a100.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH -p gpu --gres=gpu:a100:1 --mem=64G
-#SBATCH --time=10:00
-
-cd lexer && make
--- bench-h100.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH -p gpu --gres=gpu:h100:1 --mem=64G
-#SBATCH --time=1:00:00
-
-make
