0ef3ffce1c8e344ed3a625a4caa2fd09968b1b65
Due <williamhenrichdue@gmail.com>
2024-12-22 00:22:18 +0100

    This is closer to working.

--- shell.nix
+++ shell.nix
@@ -1,39 +1,24 @@
-# Run with `nix-shell cuda-fhs.nix`
+# Run with nix-shell cuda-shell.nix
 { pkgs ? import <nixpkgs> { config.allowUnfree = true; } }:
-(pkgs.buildFHSUserEnv {
-  name = "cuda-env";
-  targetPkgs = (pkgs: with pkgs; [ 
-    git
-    gitRepo
-    gnupg
-    autoconf
-    curl
-    procps
-    gnumake
-    util-linux
-    m4
-    gperf
-    unzip
-    linuxPackages.nvidia_x11
-    libGLU libGL
-    xorg.libXi xorg.libXmu freeglut
-    xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
-    ncurses5
-    # stdenv.cc
-    # futhark
-    gcc12
-    binutils
-    ispc
-    cudaPackages.cudatoolkit
-    cudaPackages.markForCudatoolkitRootHook
-  ]);
-  multiPkgs = pkgs: with pkgs; [ zlib ];
-  runScript = "bash";
-  profile = ''
-    export CUDA_PATH=${pkgs.cudatoolkit}
-    export LIBRARY_PATH=${pkgs.cudatoolkit}/lib
-    export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
-    export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-    export EXTRA_CCFLAGS="-I/usr/include"
-  '';
-}).env
+pkgs.mkShell {
+   name = "cuda-env-shell";
+   buildInputs = with pkgs; [
+     git gitRepo gnupg autoconf curl
+     procps gnumake util-linux m4 gperf unzip
+     cudatoolkit linuxPackages.nvidia_x11
+     libGLU libGL
+     futhark
+     cudaPackages.cuda_cudart
+     xorg.libXi xorg.libXmu freeglut
+     xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
+     ncurses5 stdenv.cc binutils
+   ];
+   shellHook = ''
+      export CUDA_PATH=${pkgs.cudatoolkit}
+      export CPATH=${pkgs.cudatoolkit}/include
+      export LIBRARY_PATH=${pkgs.cudatoolkit}/lib
+      export LD_LIBRARY_PATH=${pkgs.cudatoolkit}/lib:${pkgs.linuxPackages.nvidia_x11}/lib
+      export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+      export EXTRA_CCFLAGS="-I/usr/include"
+   '';
+}
